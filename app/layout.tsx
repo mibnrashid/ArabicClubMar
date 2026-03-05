461d3c0027cfa8a8a7fa26b27b46afaedfdd54cf
Musa Ibn Rashid <m.ibnrashid@gmail.com>
2026-03-05 10:38:31 +0300

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,40 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const ibmPlexArabic = localFont({
+  src: [
+    {
+      path: "../public/IBM_Plex_Sans_Arabic/IBMPlexSansArabic-Thin.ttf",
+      weight: "100",
+    },
+    {
+      path: "../public/IBM_Plex_Sans_Arabic/IBMPlexSansArabic-ExtraLight.ttf",
+      weight: "200",
+    },
+    {
+      path: "../public/IBM_Plex_Sans_Arabic/IBMPlexSansArabic-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "../public/IBM_Plex_Sans_Arabic/IBMPlexSansArabic-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "../public/IBM_Plex_Sans_Arabic/IBMPlexSansArabic-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "../public/IBM_Plex_Sans_Arabic/IBMPlexSansArabic-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "../public/IBM_Plex_Sans_Arabic/IBMPlexSansArabic-Bold.ttf",
+      weight: "700",
+    },
+  ],
+  variable: "--font-ibm-plex-arabic",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +50,7 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ibmPlexArabic.variable} font-sans antialiased`}
       >
         {children}
       </body>
